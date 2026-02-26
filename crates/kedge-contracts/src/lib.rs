e731fa4382b0b4437967f793640bb2be2697da6c
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-02-25 21:00:29 -0700

    chore: add proc-macro related crates

--- crates/kedge-contracts/src/lib.rs
+++ crates/kedge-contracts/src/lib.rs
@@ -1,17 +1,2 @@
 // SPDX-FileCopyrightText: 2026 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
 // SPDX-License-Identifier: MIT
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
