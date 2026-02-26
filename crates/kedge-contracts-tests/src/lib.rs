12ce1993a131fde797a2c734df74b1c2c1700ecb
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-02-25 22:06:10 -0700

    chore: added cargo-contracts-tests and proc-macro boilerplate

---
+++ crates/kedge-contracts-tests/src/lib.rs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2026 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
+// SPDX-License-Identifier: MIT
+
+use kedge_contracts;
+
+#[cfg(test)]
+mod tests {
+
+    #[kedge_contracts::requires("test")]
+    fn my_test() {}
+}
