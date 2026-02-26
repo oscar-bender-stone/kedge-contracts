5be284a84de3ed65163d72a4511333197c7067db
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-02-26 09:42:31 -0700

    refactor: add is_kedge_attr to confirm origin of called macro

--- crates/kedge-contracts/src/backends/kani.rs
+++ crates/kedge-contracts/src/backends/kani.rs
@@ -3,7 +3,26 @@
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Expr, FnArg, ItemFn, parse_macro_input};
+use syn::{Attribute, Expr, FnArg, ItemFn, parse_macro_input};
+
+/// Checks whether a given attribute
+/// comes from kedge-contracts.
+/// To avoid collisions with other crates,
+/// require that the *full path* be used
+fn is_kedge_attr(attr: &Attribute, attr_name: &str) -> bool {
+    let path = attr.path();
+
+    // Confirm there are exactly two segments,
+    // and that the first matche `kedge_contracts`
+    if path.segments.len() == 2 {
+        let first_segment = &path.segments[0];
+        let last_segment = &path.segments[1];
+
+        return first_segment.ident == "kedge_contracts" && last_segment.ident == attr_name;
+    }
+
+    false
+}
 
 pub fn contract(_args: TokenStream, input_fn: TokenStream) -> TokenStream {
     let mut input_fn = parse_macro_input!(input_fn as ItemFn);
@@ -12,26 +31,12 @@ pub fn contract(_args: TokenStream, input_fn: TokenStream) -> TokenStream {
 
     // Filter attributes
     input_fn.attrs.retain(|attr| {
-        if attr.path().is_ident("requires")
-            || attr
-                .path()
-                .segments
-                .last()
-                .map(|s| s.ident == "requires")
-                .unwrap_or(false)
-        {
+        if is_kedge_attr(attr, "requires") {
             if let Ok(expr) = attr.parse_args::<Expr>() {
                 requires_exprs.push(expr);
             }
             false
-        } else if attr.path().is_ident("ensures")
-            || attr
-                .path()
-                .segments
-                .last()
-                .map(|s| s.ident == "ensures")
-                .unwrap_or(false)
-        {
+        } else if is_kedge_attr(attr, "ensures") {
             if let Ok(expr) = attr.parse_args::<Expr>() {
                 ensures_exprs.push(expr);
             }
