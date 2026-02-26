5662661955dc9a634f1435bd37e7a15000e1b5d6
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-02-26 11:32:09 -0700

    refactor: added Kani and Flux backend structs

--- crates/kedge-contracts/src/backends/flux.rs
+++ crates/kedge-contracts/src/backends/flux.rs
@@ -4,11 +4,61 @@
 #![allow(unused)]
 extern crate flux_rs;
 
+use super::traits::{Backend, BackendOutput};
 use crate::validate::is_kedge_attr;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{Attribute, Expr, FnArg, ItemFn, parse_macro_input};
 use syn::{Pat, ReturnType};
+
+struct FluxBackend;
+
+impl Backend for FluxBackend {
+    fn generate(
+        input_fn: &ItemFn,
+        requires_exprs: &[Expr],
+        ensures_exprs: &[Expr],
+    ) -> BackendOutput {
+        let args = input_fn.sig.inputs.iter().map(|arg| match arg {
+            FnArg::Typed(pat_type) => {
+                let ty = &pat_type.ty;
+                match &*pat_type.pat {
+                    syn::Pat::Ident(p) => {
+                        let ident = &p.ident;
+                        quote! { #ident: #ty }
+                    }
+                    _ => quote! { _: #ty },
+                }
+            }
+            FnArg::Receiver(r) => quote! { #r },
+        });
+
+        let return_type = match &input_fn.sig.output {
+            ReturnType::Default => quote! { -> () },
+            ReturnType::Type(_, ty) => quote! { -> #ty },
+        };
+
+        let requires_tokens = if requires_exprs.is_empty() {
+            quote! {}
+        } else {
+            quote! { requires #(#requires_exprs)&&* }
+        };
+
+        let ensures_tokens = if ensures_exprs.is_empty() {
+            quote! {}
+        } else {
+            quote! { ensures result: #(#ensures_exprs)&&* }
+        };
+
+        let flux_attr = quote! {
+            #[cfg_attr(flux, flux_rs::spec(
+                fn(#(#args),*) #return_type #requires_tokens #ensures_tokens
+            ))]
+        };
+
+        BackendOutput::new(Some(vec![flux_attr]), None)
+    }
+}
 
 // TODO: add support for structs and enums
 // TODO: support more complex conditions
--- crates/kedge-contracts/src/backends/kani.rs
+++ crates/kedge-contracts/src/backends/kani.rs
@@ -1,10 +1,81 @@
 // SPDX-FileCopyrightText: 2026 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
 // SPDX-License-Identifier: MIT
 
+use super::traits::{Backend, BackendOutput};
 use crate::validate::is_kedge_attr;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{Attribute, Expr, FnArg, ItemFn, parse_macro_input};
+
+struct KaniBackend;
+
+impl Backend for KaniBackend {
+    fn generate(
+        input_fn: &ItemFn,
+        requires_exprs: &[Expr],
+        ensures_exprs: &[Expr],
+    ) -> BackendOutput {
+        let fn_name = &input_fn.sig.ident;
+        let mut arg_decls = Vec::new();
+        let mut arg_names = Vec::new();
+
+        for arg in &input_fn.sig.inputs {
+            if let FnArg::Typed(pat_type) = arg {
+                let pat = &pat_type.pat;
+                let ty = &pat_type.ty;
+                arg_decls.push(quote! {
+                    let #pat: #ty = kani::any();
+                });
+                arg_names.push(pat);
+            }
+        }
+
+        let mut kani_attrs = Vec::new();
+
+        let kani_requires: Vec<proc_macro2::TokenStream> = requires_exprs
+            .iter()
+            .map(|expr| {
+                quote! { #[cfg_attr(kani, kani::requires(#expr))] }
+            })
+            .collect();
+
+        kani_attrs.extend(kani_requires);
+
+        // To pass on conditions to Kani more easily,
+        // wrap the expression around a closure |result| { ... }
+        let kani_ensures: Vec<proc_macro2::TokenStream> = ensures_exprs
+            .iter()
+            .map(|expr| {
+                quote! { #[cfg_attr(kani, kani::ensures(|result| { #expr }))] }
+            })
+            .collect();
+
+        kani_attrs.extend(kani_ensures);
+
+        // Call the function,
+        // and make `result` a refernece
+        // to work with kani::ensures
+
+        let harness_name = quote::format_ident!("__harness_{}", fn_name);
+
+        let kani_harness = quote! {
+            #[cfg(kani)]
+            #[kani::proof]
+            #[allow(dead_code)]
+            fn #harness_name() {
+                #(#arg_decls)*
+
+                #(kani::assume(#requires_exprs);)*
+
+                let temp_result = #fn_name(#(#arg_names),*);
+
+                let result = &temp_result;
+            }
+        };
+
+        BackendOutput::new(Some(kani_attrs), Some(kani_harness))
+    }
+}
 
 pub fn contract(_args: TokenStream, input_fn: TokenStream) -> TokenStream {
     let mut input_fn = parse_macro_input!(input_fn as ItemFn);
