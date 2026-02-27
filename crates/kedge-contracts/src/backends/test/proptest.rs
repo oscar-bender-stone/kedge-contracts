43098d88f290fc5919fec8c5b3c827f533911abc
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-02-26 20:53:37 -0700

    fix: corrected repeat notation in quote!

--- crates/kedge-contracts/src/backends/test/proptest.rs
+++ crates/kedge-contracts/src/backends/test/proptest.rs
@@ -12,31 +12,35 @@ impl Backend for ProptestBackend {
         requires_exprs: &[syn::Expr],
         ensures_exprs: &[syn::Expr],
     ) -> kedge_contracts_core::traits::BackendOutput {
+        let fn_vis = &input_fn.vis;
         let fn_name = &input_fn.sig.ident;
-        let fn_sig = &input_fn.sig;
 
-        let harness_name = quote::format_ident!("__harness_{}", fn_name);
+        let harness_sig = &mut input_fn.sig.clone();
+
+        let harness_name = quote::format_ident!("___harness_{}", fn_name);
+
+        harness_sig.ident = harness_name;
+        harness_sig.output = syn::ReturnType::Default;
 
         let proptest_assumes = if requires_exprs.is_empty() {
             quote! {}
         } else {
-            quote! { #(::proptest::prop_assume!(#),*) }
+            quote! { #(::proptest::prop_assumes!(#requires_exprs);)* }
         };
 
         let proptest_asserts = if ensures_exprs.is_empty() {
             quote! {}
         } else {
-            quote! { #(::proptest::prop_assert!(#),*) }
+            quote! { #(::proptest::prop_assumes!(#ensures_exprs);)* }
         };
 
         let proptest_harness = quote! {
-        const _: () = {
+            #[cfg(proptest)]
             #[property_test]
-            fn #harness_name #fn_sig {
+            #harness_sig {
                 #proptest_assumes
                 #proptest_asserts
             }
-        }
         };
 
         BackendOutput::new(None, Some(proptest_harness))
